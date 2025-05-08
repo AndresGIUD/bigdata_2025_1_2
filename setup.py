25a51e2473749e706bcae8855025de4dff163b97
AndresGIUD <andres.graciano@est.iudigital.edu.co>
2025-05-08 23:48:37 +0000

    agregar al ylm instalación de dependencias

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name="edu_bigdata",
+    author="CamiloGraciano",
+    author_email="andres.graciano@est.iudigital.edu.co",
+    description="",
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests",
+        "BeautifulSoup4"
+    ] 
+) 
+
